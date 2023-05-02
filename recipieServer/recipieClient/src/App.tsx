8d001556ebfb923c34b20fab48e886b37f520a3d
Brad Leavitt <phoof05@gmail.com>
2023-05-02 12:58:17 -0600

    added recipe component with some css

--- recipieServer/recipieClient/src/App.tsx
+++ recipieServer/recipieClient/src/App.tsx
@@ -7,7 +7,20 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Book />
+    <>
+      <div className='headerbar'>
+        <h1 style={{marginTop: '0' , marginLeft: '10px'}}>Let's Cook!</h1>
+        <div className='headerbar'>
+          <button>recipe book</button>
+          <button>add recipe</button>
+        </div>
+        <div className = 'headerbar'>
+          <button>login</button>
+          <button>sign out</button>
+        </div>
+      </div>
+      <Book />
+    </>
   )
 }
 
--- recipieServer/recipieClient/src/components/book.tsx
+++ recipieServer/recipieClient/src/components/book.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import '../App.css'
+import '../App.css';
+import { Recipe } from './recipe';
 export const Book = () => {
   const [currentPage, setCurrentPage] = useState(0);
 
@@ -14,7 +15,16 @@ export const Book = () => {
   return (
     <div className='container'>
         <div className='book'>
-            <div className="left">Left Div</div>
+            <div className="left">
+              <Recipe 
+                title = {'Spaghetti and Meatballs'}
+                chefName = {'Giada De Laurentiis'}
+                servingSize = {4}
+                ingredients = {['1 pound spaghetti', '1 pound ground beef', '1 cup bread crumbs', '2 cloves garlic, minced']}
+                directions = {['Cook spaghetti according to package instructions', 'Mix beef, bread crumbs, garlic, and salt and pepper', 'Form into meatballs and cook in a pan', 'Serve meatballs on top of spaghetti']}
+                comments = {['This recipe is amazing!', 'I added some Parmesan cheese and it was even better','Oh definitely Parmesan a must have!','lorem ipsum bruh','lorem ipsum bruh','lorem ipsum bruh','lorem ipsum bruh','lorem ipsum bruh','lorem ipsum bruh','lorem ipsum bruh','lorem ipsum bruh','lorem ipsum bruh','lorem ipsum bruh','lorem ipsum bruh','lorem ipsum bruh','lorem ipsum bruh','lorem ipsum bruh','lorem ipsum bruh','lorem ipsum bruh']}
+              />
+            </div>
             <div className="right">Right Div</div>
         </div>
         <div className="sidebar">Sidebar Div</div>
